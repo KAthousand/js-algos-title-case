1ffde5c5b9cd74a00818f2bff3d62e8c61e16e0e
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-26 20:20:45 -0400

    Kyle Thousand

--- titleCase.js
+++ titleCase.js
@@ -1,11 +1,21 @@
 // TITLE CASE
 
-const titleCase = str => {
+const titleCase = (str) => {
+  let arr = [];
+  str
+    .split(" ")
+    .map((element) =>
+      element.charAt(0) !== "'"
+        ? arr.push(element.charAt(0).toUpperCase() + element.slice(1))
+        : arr.push(
+            element.replace(element.charAt(1), element.charAt(1).toUpperCase())
+          )
+    );
+  return arr.join(" ");
+};
 
-}
-
-console.log(titleCase("I'm a little tea pot"))
+console.log(titleCase("they don't make 'em like me no more"));
 
 module.exports = {
-  titleCase
-}
+  titleCase,
+};
